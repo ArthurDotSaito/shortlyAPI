db7183b738e0b10a76b74dd339f666c42da0ec97
Arthur Saito <arthur_saito1998@hotmail.com>
2023-02-26 18:45:46 -0300

    Fixing endpoint and dump

--- src/controllers/url/openShortenedUrl.js
+++ src/controllers/url/openShortenedUrl.js
@@ -3,23 +3,25 @@ import connection from "../../database/databaseConnection.js";
 export const openShortenedUrl = async(req,res) =>{
 
     const {shortUrl} = req.params
-    console.log(typeof(shortUrl))
+
+
     try {
-        const { rowCount, rows } = await connection.query(
+        const dbResponse = await connection.query(
             'SELECT originalUrl, visitCounter as "visitCount" FROM url WHERE shortUrl = $1',
             [shortUrl]
         )
-        console.log("shortUrl:", shortUrl);
-        console.log("rows:", rows);
-        console.log(rowCount)
-        if (rowCount === 0) return res.status(404).send("shortURL não encontrada")
+
+        console.log("rows", dbResponse.rows[0]);
+        console.log("rowCount", dbResponse.rowCount)
+
+        if (dbResponse.rowCount === 0) return res.status(404).send("shortURL não encontrada")
 
         await connection.query(
             "UPDATE url SET visitCounter = $1 WHERE shortUrl = $2",
-            [rows[0].visitCount + 1, shortUrl]
+            [dbResponse.rows[0].visitCount + 1, shortUrl]
         )
 
-        return res.redirect(rows[0].originalUrl)
+        return res.redirect(dbResponse.rows[0].originalUrl)
 
     } catch (err) {
         console.log(err)
