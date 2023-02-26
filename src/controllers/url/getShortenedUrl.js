2ac388def049889c44de3b8e24dc39157b80a9b5
Arthur Saito <arthur_saito1998@hotmail.com>
2023-02-25 22:02:41 -0300

    fix: returning 404 for unknow Id

--- src/controllers/url/getShortenedUrl.js
+++ src/controllers/url/getShortenedUrl.js
@@ -8,7 +8,7 @@ export const getShortenedUrl = async(req,res) =>{
         const dbResponse = await connection.query('SELECT id, shortURL as "shortUrl", originalURL as "url" FROM url WHERE id=$1',[id]);
         console.log(dbResponse.rows[0])
 
-        if(dbResponse.rowCount[0] === 0) return res.status(404).send("Id não encontrado");
+        if(dbResponse.rowCount === 0) return res.status(404).send("Id não encontrado");
 
         return res.status(200).json(dbResponse.rows[0]);
     }catch(error){
